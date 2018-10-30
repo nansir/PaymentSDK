43e794f9178921a57dd601668d8c0ac5d955e1cf
zhuyinan <445181052@qq.com>
2018-10-30 21:47:28 +0800

    Initial commit

---
+++ app/src/main/java/com/huawei/payment/sdk/demo/MainActivity.java
@@ -0,0 +1,13 @@
+package com.huawei.payment.sdk.demo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
